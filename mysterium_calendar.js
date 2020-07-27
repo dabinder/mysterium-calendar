a31cdeec5a3e26d85012a989f55ba6ef9fb4063f
Dan Binder <dbinder@patientkeeper.com>
2020-07-27 10:33:01 -0400

    add timezone selector

--- mysterium_calendar.js
+++ mysterium_calendar.js
@@ -88,6 +88,46 @@
 				}
 			}, false);
 		}
+		
+		//timezone offset option
+		let tzOffset = document.getElementById("tz_offset_selection"),
+			eventTimes = document.querySelectorAll(".simcal-event-start-time, .simcal-event-end-time");
+		
+		try { //insurance in case someone's browser explodes
+			if (tzOffset != null) {
+				let tzSelector = document.createElement("select");
+				for (i = -12; i <= 12; i++) {
+					let opt = document.createElement("option");
+					opt.value = i;
+					opt.textContent = "UTC" + (i < 0 ? i : "+" + i);
+					if (i == offset) opt.selected = true;
+					tzSelector.appendChild(opt);
+				}
+				
+				//create static text via JS so if for whatever reason the browser doesn't like it the selector won't even appear
+				let selectionTxt = document.createElement("span");
+				selectionTxt.textContent = "Select here to display times in a different timezone. Note this will only update times listed below; dates will not be updated.  ";
+				tzOffset.appendChild(selectionTxt);
+				tzOffset.appendChild(tzSelector);
+				tzSelector.addEventListener("change", function (e) {
+					eventTimes.forEach(function (eventTime) {
+						let timeStr = eventTime.dataset.eventStart || eventTime.dataset.eventEnd;
+						if (timeStr == undefined) return;
+						let eventDate = new Date(parseInt(timeStr) * 1000);
+						eventDate.setHours(eventDate.getUTCHours() + parseInt(this.value));
+						let hours = eventDate.getHours();
+						let ampm = hours >=12 ? 'pm' : 'am';
+						hours = hours % 12;
+						hours = hours ? hours : 12;
+						let minutes = eventDate.getMinutes();
+						minutes = minutes < 10 ? "0" + minutes : minutes;
+						eventTime.textContent = hours + ':' + minutes + ' ' + ampm;
+					}.bind(this));
+				}, false);
+			}
+		} catch (ex) {
+			//just stick with the default
+		}
 	}, false);
 
 	//periodically run update
